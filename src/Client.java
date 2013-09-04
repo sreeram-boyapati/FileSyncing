3a4ea884603f82e8ec91a7495db7fb2aea4a3979
Sreeram Boyapati <sreeram.boyapati2011@gmail.com>
2013-09-04 09:44:35 +0530

    Added GUI Components to the client

--- src/Client.java
+++ src/Client.java
@@ -2,19 +2,23 @@ import java.rmi.*;
 import java.awt.*;
 
 import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout.Constraints;
 
 public class Client {
 	
 	public static void main(String args[])
 	{
 		JFrame frame = new JFrame();
-		frame.setSize(1000, 760);
+		frame.setSize(800, 560);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(true);
+		frame.setResizable(false);
 		frame.setVisible(true);
 		
 		JPanel comm=new JPanel();
@@ -22,26 +26,51 @@ public class Client {
 		comm.setLayout(gLayout);
 		gLayout.setAutoCreateGaps(true);
 		gLayout.setAutoCreateContainerGaps(true);
-		JTextArea send_msg = new JTextArea(3, 5);
-		JTextArea recv_msg = new JTextArea(3, 5);
-		JTextField send_message = new JTextField("Send Message");
-		JTextField recv_message = new JTextField("Recieve Message");
+		JTextArea send_msg = new JTextArea();
+		send_msg.setLineWrap(true);
+		JTextArea recv_msg = new JTextArea();
+		recv_msg.setLineWrap(true);
+		JButton send=new JButton();
+		JButton recv=new JButton();
+		send.setText("Send Message");
+		recv.setText("Recieve Message");
+		JLabel send_message = new JLabel("Send Message");
+		JLabel recv_message = new JLabel("Recieve Message");
+		JLabel createFile_label =new JLabel("Create File");
+		JTextField Name_of_file=new JTextField("Enter Name of file");
+		JButton Status=new JButton("Create File");
 		gLayout.setVerticalGroup(gLayout.createSequentialGroup()
-			.addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				.addComponent(send_message)
-				.addComponent(send_msg))
-			.addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				.addComponent(recv_message)
-				.addComponent(recv_msg)));
+			.addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+					.addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(send_message)
+						.addComponent(send_msg))
+					.addComponent(send))
+			.addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+					.addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(recv_message)
+						.addComponent(recv_msg))
+					.addComponent(recv))
+			.addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+					.addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(createFile_label)
+						.addComponent(Name_of_file))
+					.addComponent(Status)));
 		gLayout.setHorizontalGroup(gLayout.createSequentialGroup()
 				.addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
 						.addComponent(send_message)
-						.addComponent(recv_message))
+						.addComponent(recv_message)
+						.addComponent(createFile_label))
 				.addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
 						.addComponent(send_msg)
-						.addComponent(recv_msg)));
+						.addComponent(recv_msg)
+						.addComponent(Name_of_file))
+				.addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+						.addComponent(send)
+						.addComponent(recv)
+						.addComponent(Status)));
 		comm.setLayout(gLayout);
 		comm.setVisible(true);
+		comm.setPreferredSize(new Dimension(500, 650));
 		frame.getContentPane().add(comm);
 		frame.setVisible(true);
 	}
